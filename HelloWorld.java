fe591c71995adac0562fa2cf87a65918bce23158
Milky19 <97377003+Milky19@users.noreply.github.com>
2023-01-11 20:49:30 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,16 +35,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello, good morning");
-    System.out.println("Hello, good morning");
-     System.out.println("Hello, good morning");
-    System.out.println("Hello, good morning");
-     System.out.println("Hello, good morning");
-    System.out.println("Hello, good morning");
-     System.out.println("Hello, good morning");
-    System.out.println("Hello, good morning");
-    System.out.println("Hello, good morning");
-     System.out.println("Hello, good morning");
-    System.out.println("Hello, good morning");
   }
 }
